3f88b80d0708d60d06427e2ef68935488bd64fc3
wanhee27 <chowh727@gmail.com>
2024-02-02 08:04:01 +0900

    [feat] 이력서 상세조회,수정,삭제 API

--- src/routes/resumes.router.js
+++ src/routes/resumes.router.js
@@ -4,7 +4,7 @@ import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-// 이력서 생성
+// 이력서 생성 API
 router.post("/resumes", authMiddleware, async (req, res, next) => {
   const { title, content } = req.body;
   const { userId } = req.user;
@@ -35,6 +35,7 @@ router.get("/resumes", async (req, res, next) => {
       title: true,
       content: true,
       user: { select: { name: true } },
+      status: true,
       createdAt: true,
       updatedAt: true
     },
@@ -46,7 +47,6 @@ router.get("/resumes", async (req, res, next) => {
   return res.status(200).json({ data: resume });
 });
 // 이력서 상세 조회 API
-// 이력서 ID, 이력서 제목, 자기소개, 작성자명, 이력서 상태, 작성 날짜 조회하기
 router.get("/resumes/:resumeId", async (req, res, next) => {
   const { resumeId } = req.params;
   const resume = await prisma.resumes.findFirst({
@@ -57,6 +57,7 @@ router.get("/resumes/:resumeId", async (req, res, next) => {
       title: true,
       content: true,
       user: { select: { name: true } },
+      status: true,
       createdAt: true,
       updatedAt: true
     }
@@ -65,7 +66,49 @@ router.get("/resumes/:resumeId", async (req, res, next) => {
 });
 
 // 이력서 수정 API
+router.patch("/resumes/:resumeId", authMiddleware, async (req, res, next) => {
+  const { resumeId } = req.params;
+  const { userId } = req.user;
+  const { title, content, status } = req.body;
+
+  const resume = await prisma.resumes.findFirst({
+    where: { resumeId: +resumeId }
+  });
+  if (!resume) {
+    return res.status(404).json({ message: "이력서 조회에 실패하였습니다." });
+  }
+  if (userId !== resume.userId) {
+    return res.status(401).json({ message: "이력서를 수정할 권한이 없습니다." });
+  }
+
+  await prisma.resumes.update({
+    data: { title, content, status },
+    where: { resumeId: +resumeId }
+  });
+
+  return res.status(200).json({ message: "이력서 수정사항이 저장되었습니다." });
+});
 
 // 이력서 삭제 API
+router.delete("/resumes/:resumeId", authMiddleware, async (req, res, next) => {
+  const { resumeId } = req.params;
+  const { userId } = req.user;
+
+  const resume = await prisma.resumes.findFirst({
+    where: { resumeId: +resumeId }
+  });
+  if (!resume) {
+    return res.status(404).json({ message: "이력서 조회에 실패하였습니다." });
+  }
+  if (userId !== resume.userId) {
+    return res.status(401).json({ message: "이력서를 수정할 권한이 없습니다." });
+  }
+
+  await prisma.resumes.delete({
+    where: { resumeId: +resumeId }
+  });
+
+  return res.status(200).json({ message: "이력서가 삭제되었습니다." });
+});
 
 export default router;
