4310a9646f8a2deab93e13e76a6eee3014d87e85
wanhee27 <chowh727@gmail.com>
2024-02-02 07:13:32 +0900

    [feat] 이력서 생성API 에러메세지

--- src/routes/resumes.router.js
+++ src/routes/resumes.router.js
@@ -9,6 +9,12 @@ router.post("/resumes", authMiddleware, async (req, res, next) => {
   const { title, content } = req.body;
   const { userId } = req.user;
 
+  if (!title) {
+    return res.status(401).json({ message: "제목을 입력해주세요." });
+  }
+  if (!content) {
+    return res.status(401).json({ message: "내용을 입력해주세요" });
+  }
   const resume = await prisma.resumes.create({
     data: {
       userId: +userId,
@@ -16,11 +22,29 @@ router.post("/resumes", authMiddleware, async (req, res, next) => {
       content
     }
   });
+
   return res.status(201).json({ message: "이력서가 등록되었습니다." });
 });
 
 // 모든 이력서 조회 API
-
+router.get("/resumes", async (req, res, next) => {
+  const resume = await prisma.resumes.findMany({
+    select: {
+      resumeId: true,
+      userId: true,
+      title: true,
+      content: true,
+      user: { select: { name: true } },
+      createdAt: true,
+      updatedAt: true
+    },
+    // QueryString 왜 사용하는지 모르겠음.
+    orderBy: {
+      createdAt: "desc"
+    }
+  });
+  return res.status(200).json({ data: resume });
+});
 // 이력서 상세 조회 API
 
 // 이력서 수정 API
